ceeaca7c4e5c7545e364aea3d5bd2c57fb1d480a
Pranav1291 <109536484+Pranav1291@users.noreply.github.com>
2023-09-25 19:56:33 +0530

    1 file modified

--- 5-keyboard/chaiaurcode.js
+++ 5-keyboard/chaiaurcode.js
@@ -1,1 +1,22 @@
-console.log('Project 5');
+const insert = document.querySelector('#insert')
+
+window.addEventListener('keydown', (e) => {
+  insert.innerHTML = `
+  <div class='color'>
+  </div>
+  <table>
+  <tr>
+    <th>Key</th>
+    <th>Keycode</th>
+    <th>Digit</th>
+  </tr>
+  <tr>
+    <td>${e.key === " " ? "Space": e.key}</td>
+    <td>${e.keyCode}</td>
+    <td>${e.code}</td>
+  </tr>
+  
+</table>
+  
+  `
+})
